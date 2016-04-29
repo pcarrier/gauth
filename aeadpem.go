b5782a17d7e1bf7c355a89f19a65056e695df767
eau <eau-code@unix4fun.net>
2016-04-29 10:21:55 +0200

    changed the encrypted storage method to AES-GCM 256 / PEM format. changed command line params.

---
+++ aeadpem.go
@@ -0,0 +1,136 @@
+package main
+
+import (
+	//"bytes"
+	//"crypto"
+	"crypto/aes"
+	"crypto/cipher"
+	//"crypto/rsa"
+	"crypto/x509"
+	"encoding/hex"
+	"encoding/pem"
+	"errors"
+	"golang.org/x/crypto/pbkdf2"
+	"golang.org/x/crypto/sha3"
+	"io"
+	"io/ioutil"
+	"strings"
+)
+
+// IsEncryptedPemFile take a filename and try to verify if it's a PEM format
+// file. The function rely on x509.IsEncryptedPEMBlock() function and return a
+// bool.
+func IsEncryptedPemFile(file string) bool {
+	fileBuf, err := ioutil.ReadFile(file)
+	if err != nil {
+		return false
+	}
+
+	pemBlockBuf, _ := pem.Decode(fileBuf)
+	return x509.IsEncryptedPEMBlock(pemBlockBuf)
+}
+
+// DecryptPEMBlock takes a password encrypted PEM block and the password used to
+// encrypt it and returns a slice of decrypted DER encoded bytes. It inspects
+// the DEK-Info header to determine the algorithm used for decryption. If no
+// DEK-Info header is present, an error is returned. If an incorrect password
+// is detected an IncorrectPasswordError is returned.
+func AEADDecryptPEMBlock(b *pem.Block, password []byte) ([]byte, error) {
+	AesHash := sha3.New256
+
+	dek, ok := b.Headers["DEK-Info"]
+	if !ok {
+		return nil, errors.New("AEADDecryptPEMBlock: no DEK-Info header in block")
+	}
+
+	dekData := strings.Split(dek, ",")
+	if len(dekData) != 3 {
+		return nil, errors.New("AEADDecryptPEMBlock: malformed DEK-Info header")
+	}
+
+	hexNonce, hexSalt := dekData[1], dekData[2]
+	nonce, err := hex.DecodeString(hexNonce)
+	if err != nil {
+		return nil, err
+	}
+
+	salt, err := hex.DecodeString(hexSalt)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(salt) != 8 {
+		return nil, errors.New("AEADDecryptPEMBlock: incorrect salt size")
+	}
+
+	/* let's PBKDF2 first.. */
+	ourKey := pbkdf2.Key(password, salt, 16384, 32, AesHash)
+	aesraw, err := aes.NewCipher(ourKey)
+	if err != nil {
+		return nil, errors.New("AEADEncryptPEMBlock: AES key setup failed: " + err.Error())
+	}
+	aesgcm, err := cipher.NewGCM(aesraw)
+	if err != nil {
+		return nil, errors.New("AEADEncryptPEMBlock: GCM failed: " + err.Error())
+	}
+
+	if len(nonce) != aesgcm.NonceSize() {
+		return nil, errors.New("AEADDecryptPEMBlock: incorrect nonce size")
+	}
+
+	plaintext, err := aesgcm.Open(nil, nonce, b.Bytes, []byte(dek))
+	if err != nil {
+		return nil, errors.New("AEADDecryptPEMBlock: wrong parameters")
+	}
+
+	return plaintext, nil
+}
+
+// AEADEncryptPEMBlock returns a PEM block of the specified type holding the
+// given DER-encoded data encrypted with AES-GCM256 algorithm, key is derived
+// using PBKDF2 on the password.
+// Header will be :
+func AEADEncryptPEMBlock(rand io.Reader, blockType string, data, password []byte) (*pem.Block, error) {
+	aesHash := sha3.New256
+
+	salt := make([]byte, 8)
+	_, err := io.ReadFull(rand, salt)
+	if err != nil {
+		return nil, errors.New("AEADEncryptPEMBlock: no rand: " + err.Error())
+	}
+
+	/* let's PBKDF2 first.. */
+	ourKey := pbkdf2.Key(password, salt, 16384, 32, aesHash)
+	aesraw, err := aes.NewCipher(ourKey)
+	if err != nil {
+		return nil, errors.New("AEADEncryptPEMBlock: AES key setup failed: " + err.Error())
+	}
+	aesgcm, err := cipher.NewGCM(aesraw)
+	if err != nil {
+		return nil, errors.New("AEADEncryptPEMBlock: GCM failed: " + err.Error())
+	}
+
+	/* this is our nonce */
+	nonce := make([]byte, aesgcm.NonceSize())
+	if _, err := io.ReadFull(rand, nonce); err != nil {
+		return nil, errors.New("AEADEncryptPEMBlock: cannot generate Nonce: " + err.Error())
+	}
+
+	/* allocate data */
+	//encrypted := make([]byte, len(data)+aesgcm.Overhead())
+
+	/* this is our header aka ad */
+	ourHeader := make(map[string]string)
+	ourHeader["Proc-Type"] = "4,ENCRYPTED"
+	ourHeader["DEK-Info"] = "AES-256-GCM" + "," + hex.EncodeToString(nonce) + "," + hex.EncodeToString(salt)
+
+	/* encrypt & authenticate */
+	encrypted := aesgcm.Seal(nil, nonce, data, []byte(ourHeader["DEK-Info"]))
+
+	/* we're done. */
+	return &pem.Block{
+		Type:    blockType,
+		Headers: ourHeader,
+		Bytes:   encrypted,
+	}, nil
+}
--- gauth.go
+++ gauth.go
@@ -2,23 +2,41 @@ package main
 
 import (
 	"bytes"
-	"crypto/aes"
-	"crypto/cipher"
+	//	"crypto/aes"
+	//	"crypto/cipher"
 	"crypto/hmac"
+	"crypto/rand"
 	"crypto/sha1"
-	"crypto/sha256"
+	//	"crypto/sha256"
 	"encoding/base32"
 	"encoding/csv"
+	"encoding/pem"
+	"flag"
 	"fmt"
 	"golang.org/x/crypto/ssh/terminal"
 	"io/ioutil"
 	"log"
 	"math/big"
+	"os"
 	"os/user"
 	"path"
 	"strings"
 	"syscall"
 	"time"
+)
+
+const (
+	CONFIG_PLAIN = ".config/gauth.csv"
+	CONFIG_PEM   = ".config/gauth.pem"
+
+	HDR_PEM = "TOPT KEYFILE"
+
+	// formatting
+	HDR_ACCT = "account"
+	HDR_PREV = "prev"
+	HDR_NEXT = "next"
+	HDR_CURR = "curr"
+	HDR_FMT  = "%-10.10s %-6s %-6s %-6s\n"
 )
 
 func TimeStamp() (int64, int) {
@@ -69,48 +87,216 @@ func authCodeOrDie(sec string, ts int64) string {
 	return str
 }
 
+//
+// default, try to open PEM file by default
+// then try the PLAIN file, WARN if plain text.
+//
+// -e : encrypt gauth.csv to gauth.pem (or file config)
+// if file already exist, ask for password, rencrypt/truncate with the same pass.
+// if file does not exist, ask for password, verify password, create the new file.
+//
+// -d : decrypt gauth.pem to gauth.csv (or file config)
+//
+
 func main() {
+	//var cfgPath string
+
 	user, e := user.Current()
 	if e != nil {
 		log.Fatal(e)
 	}
-	cfgPath := path.Join(user.HomeDir, ".config/gauth.csv")
+	/*
+		cfgPath := path.Join(user.HomeDir, ".config/gauth.csv")
 
-	cfgContent, e := ioutil.ReadFile(cfgPath)
-	if e != nil {
-		log.Fatal(e)
+		cfgContent, e := ioutil.ReadFile(cfgPath)
+		if e != nil {
+			log.Fatal(e)
+		}
+	*/
+
+	cfgPem := path.Join(user.HomeDir, CONFIG_PEM)
+	cfgPlain := path.Join(user.HomeDir, CONFIG_PLAIN)
+
+	statPlain, err := os.Stat(cfgPlain)
+	statPem, err := os.Stat(cfgPem)
+
+	// decrypt the file take the pem and generate a csv (truncate).
+	decryptFlag := flag.Bool("d", false, "decrypt config file")
+	// if the timestamp of csv is > pem and you can decrypt pem, then
+	// reencrypt the csv and replace pem, otherwise fail.
+	encryptFlag := flag.Bool("e", false, "encrypt config file")
+
+	flag.Parse()
+
+	// no config file ?!
+	if statPlain == nil && statPem == nil {
+		panic(fmt.Errorf("no topt token file present"))
+	}
+
+	// trying to encrypt and decrypt at the same time?!
+	if *encryptFlag == true && *decryptFlag == true {
+		cliErr := fmt.Errorf("-e and -d options are mutually exclusive")
+		panic(cliErr)
+
+	}
+
+	if *decryptFlag == true && statPem != nil {
+		// XXX do the decryption
+		fmt.Printf("password: ")
+		passwd, err := terminal.ReadPassword(syscall.Stdin)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("\n")
+
+		cfgContent, err := ioutil.ReadFile(cfgPem)
+		if err != nil {
+			panic(err)
+		}
+
+		cfgPemBlock, _ := pem.Decode(cfgContent)
+		if cfgPemBlock == nil || cfgPemBlock.Type != HDR_PEM {
+			panic(fmt.Errorf("invalid PEM Block\n"))
+		}
+
+		cfgPlainContent, err := AEADDecryptPEMBlock(cfgPemBlock, passwd)
+		if err != nil {
+			panic(fmt.Errorf("invalid password\n"))
+		}
+
+		err = ioutil.WriteFile(cfgPlain, cfgPlainContent, 0600)
+		if err != nil {
+			panic(err)
+		}
+
+		os.Exit(0)
+
+	}
+
+	if *encryptFlag == true && statPlain != nil {
+		// XXX do the encryption
+		fmt.Printf("password: ")
+		passwd, err := terminal.ReadPassword(syscall.Stdin)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("\n")
+
+		// first decrypt and keep the passphrase
+		if statPem != nil && IsEncryptedPemFile(cfgPem) == true {
+			cfgPemContent, err := ioutil.ReadFile(cfgPem)
+			if err != nil {
+				panic(err)
+			}
+
+			cfgPemBlock, _ := pem.Decode(cfgPemContent)
+			if cfgPemBlock == nil || cfgPemBlock.Type != HDR_PEM {
+				panic(fmt.Errorf("invalid PEM Block\n"))
+			}
+
+			_, err = AEADDecryptPEMBlock(cfgPemBlock, passwd)
+			if err != nil {
+				panic(fmt.Errorf("invalid password\n"))
+			}
+
+		} else {
+
+			fmt.Printf("retype password: ")
+			rpasswd, err := terminal.ReadPassword(syscall.Stdin)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Printf("\n")
+
+			if bytes.Compare(passwd, rpasswd) != 0 {
+				panic(fmt.Errorf("password don't match\n"))
+			}
+		}
+
+		cfgPlainContent, err := ioutil.ReadFile(cfgPlain)
+		if err != nil {
+			panic(err)
+		}
+
+		// write the new file
+		cfgContentBlock, err := AEADEncryptPEMBlock(rand.Reader, HDR_PEM, cfgPlainContent, passwd)
+		if err != nil {
+			panic(fmt.Errorf("encryption problem\n"))
+		}
+
+		cfgPemContent := pem.EncodeToMemory(cfgContentBlock)
+		err = ioutil.WriteFile(cfgPem, cfgPemContent, 0600)
+		if err != nil {
+			panic(err)
+		}
+
+		os.Exit(0)
+
+	} // end of if encryptFlag
+	var cfgContent []byte
+
+	// decrypt
+	if statPem != nil && IsEncryptedPemFile(cfgPem) == true {
+		fmt.Printf("password: ")
+		passwd, err := terminal.ReadPassword(syscall.Stdin)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("\n")
+
+		cfgPemContent, err := ioutil.ReadFile(cfgPem)
+		if err != nil {
+			panic(err)
+		}
+
+		cfgPemBlock, _ := pem.Decode(cfgPemContent)
+		if cfgPemBlock == nil || cfgPemBlock.Type != HDR_PEM {
+			panic(fmt.Errorf("invalid PEM Block\n"))
+		}
+
+		cfgContent, err = AEADDecryptPEMBlock(cfgPemBlock, passwd)
+		if err != nil {
+			panic(fmt.Errorf("invalid password\n"))
+		}
+	} else {
+		cfgContent, err = ioutil.ReadFile(cfgPlain)
+		if err != nil {
+			panic(err)
+		}
 	}
 
 	// Support for 'openssl enc -aes-128-cbc -md sha256 -pass pass:'
-	if bytes.Compare(cfgContent[:8], []byte{0x53, 0x61, 0x6c, 0x74, 0x65, 0x64, 0x5f, 0x5f}) == 0 {
-		fmt.Printf("Encryption password: ")
-		passwd, e := terminal.ReadPassword(syscall.Stdin)
-		fmt.Printf("\n")
-		if e != nil {
-			log.Fatal(e)
-		}
-		salt := cfgContent[8:16]
-		rest := cfgContent[16:]
-		salting := sha256.New()
-		salting.Write([]byte(passwd))
-		salting.Write(salt)
-		sum := salting.Sum(nil)
-		key := sum[:16]
-		iv := sum[16:]
-		block, e := aes.NewCipher(key)
-		if e != nil {
-			log.Fatal(e)
-		}
+	/*
+		if bytes.Compare(cfgContent[:8], []byte{0x53, 0x61, 0x6c, 0x74, 0x65, 0x64, 0x5f, 0x5f}) == 0 {
+			fmt.Printf("Encryption password: ")
+			passwd, e := terminal.ReadPassword(syscall.Stdin)
+			fmt.Printf("\n")
+			if e != nil {
+				log.Fatal(e)
+			}
+			salt := cfgContent[8:16]
+			rest := cfgContent[16:]
+			salting := sha256.New()
+			salting.Write([]byte(passwd))
+			salting.Write(salt)
+			sum := salting.Sum(nil)
+			key := sum[:16]
+			iv := sum[16:]
+			block, e := aes.NewCipher(key)
+			if e != nil {
+				log.Fatal(e)
+			}
 
-		mode := cipher.NewCBCDecrypter(block, iv)
-		mode.CryptBlocks(rest, rest)
-		// Remove padding
-		i := len(rest) - 1
-		for rest[i] < 16 {
-			i--
+			mode := cipher.NewCBCDecrypter(block, iv)
+			mode.CryptBlocks(rest, rest)
+			// Remove padding
+			i := len(rest) - 1
+			for rest[i] < 16 {
+				i--
+			}
+			cfgContent = rest[:i]
 		}
-		cfgContent = rest[:i]
-	}
+	*/
 
 	cfgReader := csv.NewReader(bytes.NewReader(cfgContent))
 	// Unix-style tabular
@@ -125,14 +311,17 @@ func main() {
 	prevTS := currentTS - 1
 	nextTS := currentTS + 1
 
-	fmt.Println("           prev   curr   next")
+	//fmt.Println("           prev   curr   next")
+	//fmt.Printf("%-10.10s %-6s %-6s %-6s\n", "account", "prev", "curr", "next")
+	fmt.Printf(HDR_FMT, HDR_ACCT, HDR_PREV, HDR_CURR, HDR_NEXT)
 	for _, record := range cfg {
 		name := record[0]
 		secret := normalizeSecret(record[1])
 		prevToken := authCodeOrDie(secret, prevTS)
 		currentToken := authCodeOrDie(secret, currentTS)
 		nextToken := authCodeOrDie(secret, nextTS)
-		fmt.Printf("%-10s %s %s %s\n", name, prevToken, currentToken, nextToken)
+		//fmt.Printf("%-10.10s %-6s %-6s %-6s\n", name, prevToken, currentToken, nextToken)
+		fmt.Printf(HDR_FMT, name, prevToken, currentToken, nextToken)
 	}
 	fmt.Printf("[%-29s]\n", strings.Repeat("=", progress))
 }
