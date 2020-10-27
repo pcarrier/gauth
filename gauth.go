c57414b83b103d71ab56ffe51b69d56b28a6578a
M. J. Fromberger <michael.j.fromberger@gmail.com>
2020-10-26 18:42:15 -0700

    Fix padding removal when decrypting a config file. (#36)
    OpenSSL uses PKCS#5 padding, and the decryption code was not removing it
    correctly. In some cases, this causes the last line of the decrypted config to
    be mangled and produces invalid results.
    
    To support this:
    
    - Move config loading to gauth.LoadConfigFile.
    - Inject a hook to read the user's password.
    - Add unit tests that decryption doesn't corrupt the result.
    - Update module dependencies.
    - Update Go versions in CI, and fix some config-check warnings.

--- gauth.go
+++ gauth.go
@@ -2,12 +2,8 @@ package main
 
 import (
 	"bytes"
-	"crypto/aes"
-	"crypto/cipher"
-	"crypto/sha256"
 	"encoding/csv"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"os"
 	"os/user"
@@ -21,55 +17,27 @@ import (
 )
 
 func main() {
-	user, e := user.Current()
-	if e != nil {
-		log.Fatal(e)
-	}
-	cfgPath := path.Join(user.HomeDir, ".config/gauth.csv")
-
-	cfgContent, e := ioutil.ReadFile(cfgPath)
-	if e != nil {
-		log.Fatal(e)
+	cfgPath := os.Getenv("GAUTH_CONFIG")
+	if cfgPath == "" {
+		user, err := user.Current()
+		if err != nil {
+			log.Fatal(err)
+		}
+		cfgPath = path.Join(user.HomeDir, ".config/gauth.csv")
 	}
 
-	// Support for 'openssl enc -aes-128-cbc -md sha256 -pass pass:'
-	if bytes.HasPrefix(cfgContent, []byte("Salted__")) {
-		fmt.Printf("Encryption password: ")
-		passwd, e := terminal.ReadPassword(int(syscall.Stdin))
-		fmt.Printf("\n")
-		if e != nil {
-			log.Fatal(e)
-		}
-		salt := cfgContent[8:16]
-		rest := cfgContent[16:]
-		salting := sha256.New()
-		salting.Write([]byte(passwd))
-		salting.Write(salt)
-		sum := salting.Sum(nil)
-		key := sum[:16]
-		iv := sum[16:]
-		block, e := aes.NewCipher(key)
-		if e != nil {
-			log.Fatal(e)
-		}
-
-		mode := cipher.NewCBCDecrypter(block, iv)
-		mode.CryptBlocks(rest, rest)
-		// Remove padding
-		i := len(rest) - 1
-		for rest[i] < 16 {
-			i--
-		}
-		cfgContent = rest[:i]
+	cfgContent, err := gauth.LoadConfigFile(cfgPath, getPassword)
+	if err != nil {
+		log.Fatalf("Loading config: %v", err)
 	}
 
 	cfgReader := csv.NewReader(bytes.NewReader(cfgContent))
 	// Unix-style tabular
 	cfgReader.Comma = ':'
 
-	cfg, e := cfgReader.ReadAll()
-	if e != nil {
-		log.Fatal(e)
+	cfg, err := cfgReader.ReadAll()
+	if err != nil {
+		log.Fatalf("Decoding CSV: %v", err)
 	}
 
 	currentTS, progress := gauth.IndexNow()
@@ -80,10 +48,16 @@ func main() {
 		name, secret := record[0], record[1]
 		prev, curr, next, err := gauth.Codes(secret, currentTS)
 		if err != nil {
-			log.Fatalf("Code: %v", err)
+			log.Fatalf("Generating codes: %v", err)
 		}
 		fmt.Fprintf(tw, "%s\t%s\t%s\t%s\n", name, prev, curr, next)
 	}
 	tw.Flush()
 	fmt.Printf("[%-29s]\n", strings.Repeat("=", progress))
+}
+
+func getPassword() ([]byte, error) {
+	fmt.Printf("Encryption password: ")
+	defer fmt.Println()
+	return terminal.ReadPassword(int(syscall.Stdin))
 }
--- gauth/gauth_test.go
+++ gauth/gauth_test.go
@@ -1,6 +1,7 @@
 package gauth_test
 
 import (
+	"bytes"
 	"testing"
 
 	"github.com/pcarrier/gauth/gauth"
@@ -26,5 +27,46 @@ func TestCodes(t *testing.T) {
 		} else if got != test.want {
 			t.Errorf("Code(%q, %d): got %q, want %q", test.secret, test.index, got, test.want)
 		}
+	}
+}
+
+//go:generate openssl enc -aes-128-cbc -md sha256 -pass pass:x -in testdata/plaintext.csv -out testdata/encrypted.csv
+
+func TestLoadConfig(t *testing.T) {
+
+	// To update test data, edit testdata/plaintext.csv as desired,
+	// then run go generate ./...
+	// If you change the passphrase, update getPass below.
+	//
+	// For this test, the contents don't actually matter.
+
+	var calledGetPass bool
+
+	getPass := func() ([]byte, error) {
+		calledGetPass = true
+		return []byte("x"), nil
+	}
+
+	// Load the plaintext configuration file, and verify that we did not try to
+	// decrypt its content.
+	plain, err := gauth.LoadConfigFile("testdata/plaintext.csv", getPass)
+	if err != nil {
+		t.Fatalf("Loading plaintext config: %v", err)
+	} else if calledGetPass {
+		t.Error("Loading plaintext unexpectedly called getPass")
+		calledGetPass = false
+	}
+
+	// Load the encrypted configuration file, and verify that we were able to
+	// decrypt it successfully.
+	enc, err := gauth.LoadConfigFile("testdata/encrypted.csv", getPass)
+	if err != nil {
+		t.Fatalf("Loading encrypted config: %v", err)
+	} else if !calledGetPass {
+		t.Error("Loading encrypted did not call getPass")
+	}
+
+	if !bytes.Equal(plain, enc) {
+		t.Errorf("Decrypted not equal to plaintext:\ngot  %+v\nwant %+v", enc, plain)
 	}
 }
